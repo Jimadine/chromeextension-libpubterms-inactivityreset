6dc32a2111a100071a77cfee3f84824ef31bb62b
Jim Adamson <jim.adamson@york.ac.uk>
2023-05-15 12:39:10 +0100

    Options page: refactor Reset button & associated Javascript
    Set the default values in one place. These were previously set in both options.html (for the purposes of the Reset button) & options.js (for setting the default values on new installations)

--- options/options.js
+++ options/options.js
@@ -1,5 +1,7 @@
 // Saves options using chrome.storage.local
 var storage = chrome.storage.local;
+var detection_interval_default = '30';
+var grace_period_default = '30';
 
 function saveOptions() {
   var statusEl = document.getElementById("status");
@@ -24,14 +26,16 @@ function saveOptions() {
 function restoreOptions() {
   var statusEl = document.getElementById("status");
   storage.get(['detection_interval', 'grace_period'], function(result) {
-    var storedDetectionInterval = result.detection_interval || 30
-    var storedGracePeriod = result.grace_period || 30
+    var storedDetectionInterval = result.detection_interval || detection_interval_default
+    var storedGracePeriod = result.grace_period || grace_period_default
     document.getElementById("detection_interval").value = storedDetectionInterval;
     document.getElementById("grace_period").value = storedGracePeriod;
   });
 }
 
 function resetNotify() {
+  document.getElementById("detection_interval").defaultValue = detection_interval_default;
+  document.getElementById("grace_period").defaultValue = grace_period_default;
   var statusEl = document.getElementById("status");
   statusEl.textContent = "Fields reset to defaults. Saving...";
   setTimeout(saveOptions,4000)
