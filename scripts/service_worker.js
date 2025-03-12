fac049803bb789cded636b6050100c4b10cca03f
Jim Adamson <jim.adamson@york.ac.uk>
2025-03-12 15:20:29 +0000

    service_worker.js: fix user-agent supplied parameters aren't respected
    Also:
    * reorganise code, particularly the code that handles the user-agent supplied parameters
    * remove 'started' global variable - global variables will be lost if the service worker shuts down
    * make dependency on local storage vars more robust

--- scripts/service_worker.js
+++ scripts/service_worker.js
@@ -1,16 +1,4 @@
-let browser, detectionIntervalSeconds, graceSeconds, queryStateSeconds, started, suppliedByUA;
-started = false;
-const agentString = navigator.userAgent;
-const regEx = /\sd=([0-9]{2,4}),g=([0-9]{2,4})$/;
-suppliedByUA = agentString.match(regEx);
-
-if (suppliedByUA !== null) {
-  if (parseInt(suppliedByUA[1]) < 15 || parseInt(suppliedByUA[1]) > 3000) {
-    suppliedByUA = null
-  } else if (parseInt(suppliedByUA[2]) < 15 || parseInt(suppliedByUA[2]) > 3000) {
-    suppliedByUA = null
-  }
-}
+let browser, detectionIntervalSeconds, graceSeconds, queryStateSeconds, suppliedByUA;
 
 if (navigator.userAgentData) {
   let vendors = navigator.userAgentData.brands;
@@ -19,104 +7,107 @@ if (navigator.userAgentData) {
 
 // Load the default values on extension installation
 chrome.runtime.onInstalled.addListener(() => {
-  if (!started) {
-    getVars();
-  }
+  getVars();
 })
 
 // Reload the values any time the extension first starts up
 chrome.runtime.onStartup.addListener(() => {
-  if (!started) {
-    getVars();
+  const agentString = navigator.userAgent;
+  const regEx = /\sd=([0-9]{2,4}),g=([0-9]{2,4})$/;
+  suppliedByUA = agentString.match(regEx);
+  if (suppliedByUA !== null) {
+    if (parseInt(suppliedByUA[1]) < 15 || parseInt(suppliedByUA[1]) > 3000) {
+      suppliedByUA = null
+    } else if (parseInt(suppliedByUA[2]) < 15 || parseInt(suppliedByUA[2]) > 3000) {
+      suppliedByUA = null
+    } else {
+      chrome.storage.local.set({ 'detection_interval': parseInt(suppliedByUA[1]) });
+      chrome.storage.local.set({ 'grace_period': parseInt(suppliedByUA[2]) });
+    }
   }
+  getVars();
 })
 
 // Reload the vars any time the user clicks "Save"
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-  setVars();
   getVars();
 });
 
 function getVars() {
-  started = true;
-  chrome.storage.local.get(['detection_interval', 'grace_period'], function(r) {
-    detectionIntervalSeconds = parseInt(r.detection_interval) || 30; // See https://developer.chrome.com/apps/idle#method-setDetectionInterval
-    graceSeconds = parseInt(r.grace_period) || 30; // Number of seconds after detectionIntervalSeconds to check for idleness
-    queryStateSeconds = detectionIntervalSeconds + graceSeconds; // When graceSeconds elapses we ask "has Chrome been idle for queryStateSeconds seconds. If so then clear and reset"
-    chrome.idle.setDetectionInterval(detectionIntervalSeconds);
-  })
-}
-
-function setVars() {
-  if (suppliedByUA !== null) {
-    chrome.storage.local.set({ 'detection_interval': parseInt(suppliedByUA[1]) });
-    chrome.storage.local.set({ 'grace_period': parseInt(suppliedByUA[2]) });
-  }
-}
-
-if (!started) {
-  getVars();
+  return new Promise((resolve) => {
+    chrome.storage.local.get(['detection_interval', 'grace_period'], function(r) {
+      detectionIntervalSeconds = parseInt(r.detection_interval) || 30;
+      graceSeconds = parseInt(r.grace_period) || 30;
+      queryStateSeconds = detectionIntervalSeconds + graceSeconds;
+      chrome.idle.setDetectionInterval(detectionIntervalSeconds);
+      resolve(); // Resolve the Promise when done
+    });
+  });
 }
 
 async function closeAndClear(notificationId) {
-  self.registration.getNotifications()
-    .then(notifications => {
-      const notification = notifications.find(notification => notification.tag === notificationId)
-      if (notification) {
-        notification.close()
-      }
-      chrome.idle.queryState(queryStateSeconds, function(currState) {
-        if (currState == 'idle') {
-          chrome.browsingData.remove({}, {
-            'appcache': true,
-            'cache': true,
-            'cacheStorage': true,
-            'cookies': true,
-            'downloads': true,
-            'fileSystems': true,
-            'formData': true,
-            'history': true,
-            'indexedDB': true,
-            'localStorage': true,
-            'passwords': true,
-            'webSQL': true
-          }, function() {
-            chrome.tabs.query({}, function(tabs) {
-              for (var i = 0; i < tabs.length; i++) {
-                chrome.tabs.remove(tabs[i].id);
-              }
-            })
-          })
+  getVars().then(() => {
+    self.registration.getNotifications()
+      .then(notifications => {
+        const notification = notifications.find(notification => notification.tag === notificationId)
+        if (notification) {
+          notification.close()
         }
+        chrome.idle.queryState(queryStateSeconds, function(currState) {
+          if (currState == 'idle') {
+            chrome.browsingData.remove({}, {
+              'appcache': true,
+              'cache': true,
+              'cacheStorage': true,
+              'cookies': true,
+              'downloads': true,
+              'fileSystems': true,
+              'formData': true,
+              'history': true,
+              'indexedDB': true,
+              'localStorage': true,
+              'passwords': true,
+              'webSQL': true
+            }, function() {
+              chrome.tabs.query({}, function(tabs) {
+                for (var i = 0; i < tabs.length; i++) {
+                  chrome.tabs.remove(tabs[i].id);
+                }
+              })
+            })
+          }
+        })
       })
-    })
+  })
 }
 
 chrome.idle.onStateChanged.addListener(function(newState) {
   if (newState == 'idle') {
-    chrome.history.search({
-      'text': '',
-      'maxResults': 2
-    }, function(historyItems) {
-      numVisits = historyItems.length;
-      if (numVisits > 1) {
-        let notificationId = (Math.floor(Math.random() * 1000)).toString()
-        const title = `${browser} will be reset in ${graceSeconds} seconds`
-        const options = {
-          body: `Device has been inactive for ${detectionIntervalSeconds} seconds. Move the mouse or type something within ${graceSeconds} seconds to stop ${browser} quitting`,
-          icon: '../icons/1429370080_sign-out-white-48.png',
-          tag: notificationId,
-          requireInteraction: true
+    getVars().then(() => {
+      chrome.history.search({
+        'text': '',
+        'maxResults': 2
+      }, function(historyItems) {
+        numVisits = historyItems.length;
+        if (numVisits > 1) {
+          let notificationId = (Math.floor(Math.random() * 1000)).toString()
+          const title = `${browser} will be reset in ${graceSeconds} seconds`
+          const options = {
+            body: `Device has been inactive for ${detectionIntervalSeconds} seconds. Move the mouse or type something within ${graceSeconds} seconds to stop ${browser} quitting`,
+            icon: '../icons/1429370080_sign-out-white-48.png',
+            tag: notificationId,
+            requireInteraction: true
+          }
+          self.registration.showNotification(title, options)
+          if (graceSeconds < 30) {
+            setTimeout(() => { closeAndClear(notificationId) }, graceSeconds * 1000);
+          } else {
+            chrome.alarms.create('closeAndClearAlarm' + notificationId, {
+              when: Date.now() + (graceSeconds * 1000)
+            })
+          }
         }
-        self.registration.showNotification(title, options)
-        if (graceSeconds < 30) {
-          setTimeout(() => { closeAndClear(notificationId) }, graceSeconds * 1000);
-        } else {
-          chrome.alarms.create('closeAndClearAlarm' + notificationId, {
-            when: Date.now() + (graceSeconds * 1000)
-          })
-        }
-      }
+      })
     })
   }
 })
